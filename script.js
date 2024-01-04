e2a442c9a1b00467b124377f2d7730d151ef6838
NathanC90 <ymnchao@gmail.com>
2024-01-04 16:30:54 +0800

    Add 3 varibles and 3 event listeners regarding the buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+let rainbowBtn = document.getElementById("rainbow-btn");
+let blackBtn = document.getElementById("black-btn");
+let eraseBtn = document.getElementById("erase-btn");
+
+rainbowBtn.addEventListener('click', function() {
+    alert("I got clicked!");
+})
+
+blackBtn.addEventListener('click', function() {
+    alert("black button is clicked!");
+})
+
+eraseBtn.addEventListener('click', function() {
+    alert("Erase button is clicked!");
+})
