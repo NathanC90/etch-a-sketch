8ace5ca654996ecc34f64c055a8ddcae9bef105e
NathanC90 <ymnchao@gmail.com>
2024-01-05 12:57:11 +0800

    Add clear function and revise clear button id

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let rainbowBtn = document.getElementById("rainbow-btn");
 let blackBtn = document.getElementById("black-btn");
 let eraseBtn = document.getElementById("erase-btn");
+let clearBtn = document.getElementById("clear-btn");
 
 // Input elements
 let gridInput = document.getElementById("grid-input");
@@ -10,6 +11,7 @@ let colorPicker = document.getElementById("color-picker");
 // Doodle elements
 let doodleBoard = document.getElementById("doodle-board");
 let gridSize = document.getElementById("grid-size");
+let newGrid = document.querySelectorAll(".newGrid");
 
 // show initial grid size
 gridSize.textContent = `${gridInput.value} x ${gridInput.value}`;
@@ -30,3 +32,15 @@ gridInput.addEventListener('input', function(){
     let column = gridInput.value;
     gridSize.textContent = `${row} x ${column}`;
 })
+
+function setGridSize(num) {
+    for(let i = 0; i < num * num; i++) {
+        doodleBoard.innerHTML += `<div class="newGrid"></div>`;
+    }
+}
+
+clearBtn.addEventListener('click', function(){
+    document.querySelectorAll(".newGrid").forEach(el => {
+        el.style.backgroundColor = "white";
+    });
+});
