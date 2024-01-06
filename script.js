2a32882921de7aba6ff6cc5fba907f56830bfe9e
NathanC90 <ymnchao@gmail.com>
2024-01-06 16:25:15 +0800

    Show intial 16x16 grid size

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ let currentColor = colorPicker.value;
 // Doodle elements
 let doodleBoard = document.getElementById("doodle-board");
 let gridSize = document.getElementById("grid-size");
+let numberOfGrids = gridInput.value;
 
 // show initial grid size
-gridSize.textContent = `${gridInput.value} x ${gridInput.value}`;
+gridSize.textContent = `${numberOfGrids} x ${numberOfGrids}`;
 
 // show default color
 hex.textContent = colorPicker.value;
@@ -24,6 +25,18 @@ hex.textContent = colorPicker.value;
 colorPicker.addEventListener('input', function(){
     currentColor.textContent = colorPicker.value;
 });
+
+//show current grid size
+gridInput.addEventListener('input', function(){
+    let row = gridInput.value;
+    let column = gridInput.value;
+    gridSize.textContent = `${row} x ${column}`;
+})
+
+// show default grid size
+setGridSize(numberOfGrids)
+
+
 
 // generate random colors
 function setRandomColors(){
@@ -58,33 +71,33 @@ function eraseColors(){
     }
 }
 
+// Click to set to Rainbow Mode
 rainbowBtn.addEventListener('click', function() {
     setRandomColors();
 })
 
+// Click to set to single color mode
 colorBtn.addEventListener('click', function() {
     setCurrentColor();
 })
 
+// Click to set to Erase Mode
 eraseBtn.addEventListener('click', function() {
     eraseColors();
 })
 
-//show current grid size
-gridInput.addEventListener('input', function(){
-    let row = gridInput.value;
-    let column = gridInput.value;
-    gridSize.textContent = `${row} x ${column}`;
-})
-
-function setGridSize(num) {
-    for(let i = 0; i < num * num; i++) {
-        doodleBoard.innerHTML += `<div class="newGrid"></div>`;
-    }
-}
-
+// Click to remove all background colors
 clearBtn.addEventListener('click', function(){
     document.querySelectorAll(".newGrid").forEach(el => {
         el.style.backgroundColor = "white";
     });
 });
+
+// Rearrange grid size
+function setGridSize(num) {
+    for(let i = 0; i < num * num; i++) {
+        let newGrid = document.createElement('div');
+        newGrid.className = "newGrid";
+        doodleBoard.appendChild(newGrid);
+    }
+}
